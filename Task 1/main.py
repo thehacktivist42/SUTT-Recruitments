8870e41bde03f97fc186602283cec2d29503efbf
Harivansh Mehta <harivanshmehta@Harivanshs-MacBook-Air.local>
2024-11-12 20:41:08 +0530

    Added explanatory comments

--- Task 1/main.py
+++ Task 1/main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import json
-times = {
+times = { # Dictionary for the hour number to 24-hour time conversion
         1 : 8,
         2 : 9,
         3 : 10,
@@ -11,7 +11,7 @@ times = {
         8 : 15,
         9 : 16
     }
-days = {
+days = { # Dictionary for the day abbreviation to full-form conversion
     'M' : 'Monday',
     'T' : 'Tuesday',
     'W' : 'Wednesday',
@@ -19,7 +19,7 @@ days = {
     'F' : 'Friday',
     'S' : 'Saturday'
 }
-venues = {
+venues = { # QoL Feature addition: Dictionary for the venue names
     1 : 'FD-I',
     2 : 'FD-II',
     3 : 'FD-III',
@@ -28,27 +28,27 @@ venues = {
     7 : 'Central Workshop'
 }
 
-file = pd.ExcelFile("Timetable Workbook - SUTT Task 1.xlsx")
+file = pd.ExcelFile("Timetable Workbook - SUTT Task 1.xlsx") # Generates an object from the Excel file
 
-d = {}
+d = {} # A dictionary for the multiple sheets in the Excel file. The following two lines create key value pairs for the sheet number and the respective dataframe.
 for sheet in file.sheet_names:
     d[f'{sheet}'] = pd.read_excel(file, skiprows = 1, sheet_name = sheet)
 
-subjects = {}
+subjects = {} # Main dictionary to be converted to JSON
 
-for i in d:
+for i in d: # For every sheet containing a subject, the following code is executed
     d[i].drop(0)
     temp = {}
     for j in d[i]["COURSE NO."]:
-        if j == j:
+        if j == j: # Check for non-NaN values
             temp["courseCode"] = j
             break
     subjects[i] = temp
     for j in d[i]["COURSE TITLE"]:
-        if j == j and j != "Tutorial":
+        if j == j and j != "Tutorial": # Checks for values that are non-NaN and not called "Tutorial". Only one such value exists and that is the course title.
             temp["courseTitle"] = j
             break
-    credits = {"L" : "-", "P" : "-", "U" : "-"}
+    credits = {"L" : "-", "P" : "-", "U" : "-"} # Initializes a dictionary for the credit structure. If an integer is found in the respective columns, the '-' is replaced with it.
     for j in d[i]["CREDIT"]:
         if j == j and type(j) is int:
             credits["L"] = j
@@ -59,37 +59,37 @@ for i in d:
         if j == j and type(j) is int:
             credits["U"] = j
     temp["credits"] = credits
-    secList = []
+    secList = [] # List of sections
     for sec in d[i]["SEC"]:
         if sec == sec:
             secList.append(sec)
-    sections = {sec : {"instructors" : [], "room" : '', "time" : '' } for sec in secList}
+    sections = {sec : {"instructors" : [], "room" : '', "time" : '' } for sec in secList} # Initializes a dictionary for the section structure, containing a list of instructors, room and time
     profList = []
     curSec = ''
     curTime = ''
     for sec, prof, room, time in zip(d[i]["SEC"], d[i]["INSTRUCTOR-IN-CHARGE / Instructor"], d[i]["ROOM"], d[i]["DAYS & HOURS"]):
         temp1 = {}
         if sec == sec:
-            curSec = sec
-            sections[curSec]['room'] = f"{int(room)} ({venues[int(str(int(room))[0])]})"
-            time = time.split('  ')
+            curSec = sec # If the column contains a non-NaN value, it is taken as the current section
+            sections[curSec]['room'] = f"{int(room)} ({venues[int(str(int(room))[0])]})" # The room and the location of the section is added to the sections dictionary.
+            time = time.split('  ') # The time is converted into a list containing the day and the hours
             for i in time:
-                if not i[0].isdigit():
+                if not i[0].isdigit(): # If not digit, it is the day and not the hours.
                     try:
-                        sections[curSec]['time'] += days[i]
-                        if len(time[time.index(i)+1]) == 1:
+                        sections[curSec]['time'] += days[i] # Only possible when only single character, i.e., single day exists.
+                        if len(time[time.index(i)+1]) == 1: # Only possible when only single digit, i.e., single hour exists.
                             sections[curSec]['time'] += f" {times[int(time[time.index(i)+1])]}:00 - {times[int(time[time.index(i)+1])]+1}:00  "
-                        else:
+                        else: # Considered when the class lasts for more than one hour.
                             sections[curSec]['time'] += f" {times[int(time[time.index(i)+1][0])]}:00 - {times[int(time[time.index(i)+1][-1])]+1}:00  "
-                    except:
+                    except: # Considered when the class is on multiple days.
                         for j in i.split():
                             sections[curSec]['time'] += days[j]
                             sections[curSec]['time'] += f" {times[int(time[time.index(i)+1])]}:00 - {times[int(time[time.index(i)+1])]+1}:00  "
-            sections[curSec]['time'] = sections[curSec]['time'].rstrip()
+            sections[curSec]['time'] = sections[curSec]['time'].rstrip() # Removes extra whitespace on the right side.
         if curSec != '':
-            sections[curSec]['instructors'].append(prof)
+            sections[curSec]['instructors'].append(prof) # Adds instructors to the current section until the next section is found.
     temp["sections"] = sections
 
-print(json.dumps(subjects, indent = 4)) 
+print(json.dumps(subjects, indent = 4)) # Pretty-prints the final dictionary to the terminal.
 with open('data.json', 'w', encoding = 'utf-8') as f:
-    json.dump(subjects, f, indent = 4)   
+    json.dump(subjects, f, indent = 4)  # Dumps the dictionary to data.json
